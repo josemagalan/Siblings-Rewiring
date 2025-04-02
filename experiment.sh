b612dd5ed2d061c428aae86f85406acc053538e0
josemagalan <josemagalan@users.noreply.github.com>
2025-04-02 13:19:59 +0200

    SA and GA experiment scripts for execution on supercomputing cluster

---
+++ experiment.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --job-name=exp_paralelo
+#SBATCH --output=logs/output_%A_%a.log
+#SBATCH --error=logs/error_%A_%a.log
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=4
+#SBATCH --chdir=./
+#SBATCH --time=180:00
+#SBATCH --array=1-180# Lanza 180 jobs en paralelo
+
+module load OPENMPI/4.1.6
+module load gcc/12.3.0
+module load R/4.2.2
+
+echo "Iniciando tarea $SLURM_ARRAY_TASK_ID en: $(date)"
+
+time Rscript experiment.R
+
+echo "Finalizando tarea $SLURM_ARRAY_TASK_ID en: $(date)"
